e826e11288e2680a1d58487ff85b90ba9d6467f6
brikboy <grant@hulen.com>
2016-03-06 18:24:24 -0800

    Session 005 (end)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,17 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     float maxSpeed = 20f;
+    Animator anim;
 
 	// Use this for initialization
 	void Start () {
-	
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         float move = Input.GetAxis("Horizontal");
         GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        anim.SetFloat("Speed", Mathf.Abs(move));
 	}
 }
