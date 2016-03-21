0977f2df746f28aa803c631d4a6ce55a2d10984e
brikboy <grant@hulen.com>
2016-03-20 20:16:17 -0700

    Adding distro

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour {
 
     float maxSpeed = 20f;
     Animator anim;
+    bool facingRight = true;
 
 	// Use this for initialization
 	void Start () {
@@ -16,5 +17,22 @@ public class PlayerController : MonoBehaviour {
         float move = Input.GetAxis("Horizontal");
         GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
         anim.SetFloat("Speed", Mathf.Abs(move));
+
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        } else if (move < 0 && facingRight)
+        {
+            Flip(); 
+
+        }
 	}
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x = scale.x * -1;
+        transform.localScale = scale;
+    }
 }
